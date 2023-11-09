ichteltelch/Pile

commit 40e1408a9efa23043e7c848199209e81142dd689

Fixed rare edge case involving transactions that don't invalidate

src/pile/impl/PileImpl.java
@@ -1517,10 +1517,9 @@ implements Pile<E>, HasAssociations.Mixin
 			__beginTransaction(invalidate);
 		}
 
-		if(wasValid)
-			__scheduleRecomputation(true);
 
-
+		boolean recomputationWasScheduledOrOngoing;
+		boolean recomputationWasScout;
 		synchronized (mutex) {
 			if(_thisDependsOn==null && !_thisDependsOn.contains(d)) {
 				System.err.println(d.dependencyName()+" is not a dependency of "+dependencyName());
@@ -1545,13 +1544,17 @@ implements Pile<E>, HasAssociations.Mixin
 				}
 			}
 			dependencyTransactions++;
-
-
+			recomputationWasScheduledOrOngoing = recomputationTransactions>=1;
+			recomputationWasScout = nextRecomputationIsScout || (ongoingRecomputation!=null && ongoingRecomputation.isDependencyScout());
 		}
 		if(wasValid) {
 			fireDeepRevalidate();
 		}
+		
 		cancelPendingRecomputation(true);
+
+		if(wasValid | recomputationWasScheduledOrOngoing)
+			__scheduleRecomputation(true, recomputationWasScout && !wasValid);
 
 		//		boolean scout = false;
 		//		if(recompute!=null && recompute.useDependencyScoutingOnBeginningChange() && recompute.useDependencyScoutingIfInvalid(null)) {
@@ -1576,6 +1579,66 @@ implements Pile<E>, HasAssociations.Mixin
 		////			scheduleRecomputation(false);
 		//			startPendingRecompute(true);
 		//		}
+	}
+	@Override
+	public void escalateDependencyChange(Dependency d) {
+		//		if(DE && dc!=null) dc.dependencyBeginsChanging(this, d, isValidAsync());			
+		//		boolean selWasValid = 
+
+		assert !Thread.holdsLock(mutex);
+		////synchronized (schedulingMutex) 
+
+
+		boolean recomputationWasScheduledOrOngoing;
+//		boolean recomputationWasScout;
+		boolean wasValid;
+		synchronized (mutex) {
+			if(_thisDependsOn==null && !_thisDependsOn.contains(d)) {
+				System.err.println(d.dependencyName()+" is not a dependency of "+dependencyName());
+			}
+			wasValid = valid;
+			if(valid)
+				moveValueToOldValue();
+
+			invalidated=false;
+			if(changingDependencies != null && changingDependencies.contains(d)) {
+				try {
+					throw new IllegalStateException("Dependency was not changing");
+				}catch(IllegalStateException x) {
+					x.printStackTrace();
+				}
+			}
+			recomputationWasScheduledOrOngoing = recomputationTransactions>=1;
+//			recomputationWasScout = nextRecomputationIsScout || (ongoingRecomputation!=null && ongoingRecomputation.isDependencyScout());
+
+			if(informed!=null && wasValid) {
+				assert !Thread.holdsLock(informQueue);
+				// assert !Thread.holdsLock(informRunnerMutex);
+				if(ET_TRACE && traceEnabledFor(this))trace("schedule informing of dependers that I have become invalid");
+				//			StackTraceElement[] cause = Thread.currentThread().getStackTrace();
+				synchronized (informQueue) {
+					informQueue.add(()->{
+						if(ET_TRACE && traceEnabledFor(this))trace("now informing dependers that I have become invalid");
+						if(informed==null)
+							return;
+						//					cause.clone();
+						for(Depender d2: informed) {
+							d2.escalateDependencyChange(this);
+						}
+
+					});
+				}
+			}
+			
+		}
+		fireDeepRevalidate();
+		
+		cancelPendingRecomputation(true);
+
+		if(recomputationWasScheduledOrOngoing)
+			__scheduleRecomputation(true);
+
+		
 	}
 
 

src/pile/aspect/Depender.java
@@ -110,6 +110,14 @@ public interface Depender {
 	 * @param d
 	 */
 	void dependencyBeginsChanging(Dependency d, boolean wasValid, boolean invalidate);
+
+	/**
+	 * Called by a dependency that has already notified this {@link Depender} that it 
+	 * is in a transaction, but has not become invalid until now.
+	 * @param newlyInvalidDependency
+	 */
+	public void escalateDependencyChange(Dependency newlyInvalidDependency);
+
 	/**
 	 * Called by a dependency when it has finished a transaction that might have changed it
 	 * <br>

src/pile/impl/AbstractReadListenDependency.java
@@ -599,7 +599,8 @@ ListenValue.Managed{
 										if(ET_TRACE && traceEnabledFor(this))trace("now informing dependers "+d);
 										d.dependencyBeginsChanging(this, wasValid, moveValueToOldValue);
 									}else {
-										if(ET_TRACE && traceEnabledFor(this))trace("was already informed dependers "+d);
+										if(wasValid && moveValueToOldValue)
+											d.escalateDependencyChange(this);
 									}
 								}
 							}

src/pile/impl/SealPile.java
@@ -296,6 +296,11 @@ public class SealPile<E> extends PileImpl<E> implements Sealable<E> {
 		public void dependencyBeginsChanging(Dependency d, boolean wasValid, boolean invalidate) {
 			SealPile.this.dependencyBeginsChanging(d, wasValid, invalidate);				
 		}
+		
+		@Override
+		public void escalateDependencyChange(Dependency newlyInvalidDependency) {
+			SealPile.this.escalateDependencyChange(newlyInvalidDependency);
+		}
 
 		@Override
 		public void deepDestroy() {

commit e35b22743a22424b0356e57e9fed4e81a253f861

Commit 40e1408 broke something, hopefully this time I got it right

src/pile/impl/PileImpl.java
@@ -1517,6 +1517,8 @@ implements Pile<E>, HasAssociations.Mixin
 			__beginTransaction(invalidate);
 		}
 
+		if(wasValid)
+			__scheduleRecomputation(true);
 
 		boolean recomputationWasScheduledOrOngoing;
 		boolean recomputationWasScout;
@@ -1553,8 +1555,8 @@ implements Pile<E>, HasAssociations.Mixin
 		
 		cancelPendingRecomputation(true);
 
-		if(wasValid | recomputationWasScheduledOrOngoing)
-			__scheduleRecomputation(true, recomputationWasScout && !wasValid);
+		if(recomputationWasScheduledOrOngoing)
+			__scheduleRecomputation(true, recomputationWasScout || this.recompute.useDependencyScouting() && !wasValid);
 
 		//		boolean scout = false;
 		//		if(recompute!=null && recompute.useDependencyScoutingOnBeginningChange() && recompute.useDependencyScoutingIfInvalid(null)) {